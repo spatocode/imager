1ffbfbe26bc37d731f4697fd2aff6555da612831
Ekene Izukanne <ekeneizukanne@gmail.com>
2021-12-17 21:00:29 +0100

    update

--- hello.py
+++ hello.py
@@ -5,16 +5,14 @@ from subprocess import call
 import shutil
 
 
-folder_path = sys.argv[1]
-folder_name = folder_path.split('/')[-1:]
-folder_name = folder_name[0]
-desktop_path = folder_path.split('/')[:3]
+file_path = sys.argv[1]
+folder_path = file_path.split(os.path.basename(file_path))[0]
+desktop_path = file_path.split('/')[:3]
 desktop_path = '/'.join(desktop_path)+'/Desktop/'
 
-
-
-
 air_directory = os.path.join(desktop_path, 'AIR Low-Res Sets/')
+
+ds_store = os.path.join(folder_path, '.DS_Store')
 
 
 try:
@@ -25,41 +23,19 @@ except:
 print(folder_path)
 
 try:
-	os.system("rm -r {}/.DS_Store".format(folder_path))
+	if os.path.exists(ds_store):
+		os.system("rm -r {}/.DS_Store".format(folder_path))
 except:
 	print('Deleted DS store')
 
-file_names = os.listdir(folder_path)
 
+img = Image.open(file_path)
+basewidth = 800
 
-try:
-	file_names.remove('.DS_Store')
-except:
-	print('DS Store removed')
-
-low_res_directory = os.path.join(air_directory, folder_name)
-
-
-
-try:
-	os.mkdir(low_res_directory)
-except:
-	print('Low Res folder already exists')
-	try:
-		shutil.rmtree(low_res_directory)
-		os.mkdir(low_res_directory)
-	except:
-		print('Deleted and re-added folder')
-
-for i in range(len(file_names)):
-	file_path = '{}/'.format(folder_path)+file_names[i]
-	img = Image.open(file_path)
-	basewidth = 800
-
-	wpercent = (basewidth / float(img.size[0]))
-	hsize = int((float(img.size[1]) * float(wpercent)))
-	img = img.resize((basewidth, hsize), Image.ANTIALIAS)
-	img.save('{}/{}'.format(low_res_directory,file_names[i]))
+wpercent = (basewidth / float(img.size[0]))
+hsize = int((float(img.size[1]) * float(wpercent)))
+img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+img.save(os.path.join(air_directory, os.path.basename(file_path)))
 print('Complete...!')
 
 
--- src/render.js
+++ src/render.js
@@ -8,9 +8,7 @@ const {PythonShell} = require('python-shell')
 var folder = document.getElementById("myInput");
 folder.onchange=function(){
   var files = folder.files;
-  var split_path = files[0].path.split('/');
-  var pop_path = split_path.pop()
-  var file_path = split_path.join('/')
+  var file_path = files[0].path
 
   let pythonPath = remote.app.isPackaged ? path.join(process.resourcesPath, "python", "bin", "python3.9") : path.join(process.cwd(), "python", "bin", "python3.9")
   let scriptPath = remote.app.isPackaged ? process.resourcesPath : process.cwd()
@@ -21,7 +19,7 @@ folder.onchange=function(){
     scriptPath: scriptPath,
     args: [file_path]
   };
-
+  console.log("----------", file_path)
   PythonShell.run('hello.py', options, function (err, result){
     if (err) throw err;
     console.log('result: ', result.toString());
