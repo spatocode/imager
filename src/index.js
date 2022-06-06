d387dc24d34806268ef1d4f579a5a29c7aada1a2
Ekene Izukanne <ekeneizukanne@gmail.com>
2022-06-06 15:47:20 +0100

    update to fix issue

--- src/index.js
+++ src/index.js
@@ -6,17 +6,21 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    autoHideMenuBar: true,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
-    }
+      enableRemoteModule: true
+    },
   });
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  if (!app.isPackaged) {
+    mainWindow.webContents.openDevTools();
+  }
 };
 
 // This method will be called when Electron has finished
@@ -40,6 +44,3 @@ app.on('activate', () => {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
--- src/render.js
+++ src/render.js
@@ -1,15 +1,91 @@
 const path = require('path')
-const { remote } = require('electron')
-
+const { remote, shell } = require('electron')
 const {PythonShell} = require('python-shell')
 
+const dropZone = document.getElementById("dropZone");
+const frameTwo = document.getElementById("frameTwo");
+const frameThree = document.getElementById("frameThree");
+const fileLabel = document.getElementById("fileLabel");
+const textInput = document.getElementById("textInput");
+const hiddenInput = document.getElementById("hiddenInput");
+const button = document.querySelector("button");
+const bar = document.getElementById("bar");
+const progress = document.getElementById("progress");
 
+const file_paths = []
+let savedDirectory = ""
+let interval
 
-var folder = document.getElementById("myInput");
-folder.onchange=function(){
-  var files = folder.files;
-  var file_path = files[0].path
+hiddenInput.onchange = function() {
+  let files = hiddenInput.files;
+  for(let i=0; i < files.length; i++) {
+    file_paths.push(files[i].path)
+  }
+  if (file_paths.length > 0) {
+    initiateFrameTwo()
+  }
+}
 
+dropZone.onclick = function() {
+  hiddenInput.click()
+}
+
+dropZone.ondrop = function(e) {
+  e.preventDefault()
+  if (e.dataTransfer.items) {
+    for (let i=0; i < e.dataTransfer.items.length; i++) {
+      if (e.dataTransfer.items[i].kind === 'file') {
+        let file = e.dataTransfer.items[i].getAsFile()
+        console.log(file.path)
+        file_paths.push(file.path)
+      }
+    }
+    initiateFrameTwo()
+  }
+}
+
+dropZone.ondragover = function (e) {
+  e.preventDefault();
+}
+
+textInput.onchange = function(e) {
+  progress.style.visibility = "visible"
+  initiateProgressBar()
+  processFile(file_paths, e.target.value)
+}
+
+button.onclick = function() {
+  progress.style.visibility = "visible"
+  initiateProgressBar()
+  processFile(file_paths, textInput.value)
+}
+
+frameThree.onclick = function() {
+  shell.openPath(savedDirectory)
+}
+
+function initiateProgressBar() {
+  let marginLeft = 1;
+  interval = setInterval(function() {
+    if (marginLeft >= 80) {
+      marginLeft = 1
+      bar.style.marginLeft = marginLeft + "%";
+    } else {
+      marginLeft++;
+      bar.style.marginLeft = marginLeft + "%";
+    }
+  }, 10);
+}
+
+function initiateFrameTwo () {
+  if (file_paths.length > 0) {
+    dropZone.style.display = "none"
+    frameTwo.style.display = "block"
+    fileLabel.textContent = `${file_paths.length} FILES WILL BE PROCESSED`
+  }
+}
+
+function processFile(files, folderName) {
   let pythonPath = remote.app.isPackaged ? path.join(process.resourcesPath, "python", "bin", "python3.9") : path.join(process.cwd(), "python", "bin", "python3.9")
   let scriptPath = remote.app.isPackaged ? process.resourcesPath : process.cwd()
   let options = {
@@ -17,11 +93,16 @@ folder.onchange=function(){
     pythonPath:  pythonPath,
     pythonOptions: ['-u'],
     scriptPath: scriptPath,
-    args: [file_path]
+    args: [...files, folderName]
   };
-  console.log("----------", file_path)
+
   PythonShell.run('hello.py', options, function (err, result){
     if (err) throw err;
-    console.log('result: ', result.toString());
+    console.log('Result: ', result.toString());
+    home_path = files[0].split('/').slice(0, 3).join("/")
+    savedDirectory = path.join(home_path, 'Desktop', 'Imager', folderName)
+    frameTwo.style.display = "none"
+    frameThree.style.display = "block"
+    clearInterval(interval)
   });
 }
